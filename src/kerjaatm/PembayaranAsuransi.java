633638f390a2afae0f3900a2f83a21c1d049c021
Rindu Mustika Pratiwi <45808342+rindumustika@users.noreply.github.com>
2019-01-03 10:42:28 +0700

    benerin asuransi

--- src/kerjaatm/PembayaranAsuransi.java
+++ src/kerjaatm/PembayaranAsuransi.java
@@ -12,57 +12,61 @@ import java.util.Scanner;
  * @author user
  */
 public class PembayaranAsuransi extends Transaction {
-   private double amount; // amount to withdraw
-   private Keypad keypad; // reference to keypad
+   private double amount;
+   private Keypad keypad;
+   private final static int CANCELED = 6;
    private DataAsuransi asuransi;
 
-   // constant corresponding to menu option to cancel
-   private final static int CANCELED = 6;
-
-   // Withdrawal constructor
+   
    public PembayaranAsuransi(int userAccountNumber, Screen atmScreen, 
-      BankDatabase atmBankDatabase, Keypad atmKeypad, DataAsuransi atmAsuransi) {
-
-      // initialize superclass variables
-      super(userAccountNumber, atmScreen, atmBankDatabase);
-      keypad = atmKeypad;
-      asuransi = atmAsuransi;
-        }
+      BankDatabase atmBankDatabase, Keypad atmKeypad, DataAsuransi atmAsuransi) 
+      {
+            super(userAccountNumber, atmScreen, atmBankDatabase);
+            keypad = atmKeypad;
+            asuransi = atmAsuransi;
+      }
 
     @Override
     public void execute() {
-      BankDatabase bankDatabase = getBankDatabase();
+      //BankDatabase bankDatabase = getBankDatabase();
       Screen screen = getScreen();
-      if(cl.printMsg() == 1){screen.displayMessage("Input your Insurance Policy number :");}
-      if(cl.printMsg() == 2){screen.displayMessage("Masukkan nomor Polis Asuransi anda :");}
-      int noPolis = keypad.getInput();
-       amount = jumlahTransfer();
-       if(getBankDatabase().getAvailableBalance(getAccountNumber()) > amount){
-           getBankDatabase().transfer(getAccountNumber(), amount,noPolis);
-           if(cl.printMsg() == 1){screen.displayMessageLine("Insurance payment in amount of: "+amount+" Policy Number: "+noPolis+" is successful");}
-           if(cl.printMsg() == 2){screen.displayMessageLine("Pembayaran asuransi sebesar: "+amount+" Nomor Polis: "+noPolis+" Telah berhasil");}
-       }
-       else {
-           if(cl.printMsg() == 1){screen.displayMessageLine("Insufficient balance..");}
-           if(cl.printMsg() == 2){screen.displayMessageLine("Saldo tidak mencukupi..");}
-       }
-    }
-    
-    private double jumlahTransfer(){
-    Screen screen = getScreen(); // get reference to screen
-
-      // display the prompt
-    if(cl.printMsg() == 1){screen.displayMessage("\nInput payment amount: " + "CENTS (or 0 to cancel): ");}
-    if(cl.printMsg() == 2){screen.displayMessage("\nMasukan Jumlah Premi: " + "SEN (ketik 0 untuk cancel): ");}
-    int input = keypad.getInput(); // receive input of deposit amount
+      Scanner in = new Scanner(System.in);   
+      int noPolis = 0;
       
-      // check whether the user canceled or entered a valid amount
-    if (input == CANCELED) {
-         return CANCELED;
-      }else {
-         return (double) input / 100; // return dollar amount
+      do{
+        screen.displayMessage("Masukkan nomor Polis Asuransi anda :");  
+        try{
+            noPolis = in.nextInt();
+            break;
+        }
+        catch(Exception e)
+        {
+            System.out.println("!! INPUT HANYA BOLEH ANGKA !!\n");
+            in.nextLine();
+        }
+      }while(noPolis<100);
+      
+      if(asuransi.cekNoPolis(noPolis)){
+            amount = asuransi.getJumlahPremi(noPolis);
+            if(getBankDatabase().getAvailableBalance(getAccountNumber()) > amount)
+            {
+                getBankDatabase().debit(getAccountNumber(), amount);
+                screen.displayMessageLine("\n------------------------------------------"
+                   + "\nTRANSAKSI BERHASIL"
+                   + "\n\nPembayaran Asuransi"
+                   + "\nNomor Polis     : "+noPolis+""
+                   + "\nNama            : "+asuransi.getNamaPelAsuransi(noPolis)+""
+                   + "\nJumlah Premi    : $"+amount+" "
+                   + "\n\nTerimakasih telah menggunakan layanan kami"
+                   + "\n------------------------------------------\n\n");
+            } else {
+              screen.displayMessageLine("Saldo tidak mencukupi..");
+            }
+      } else {
+        screen.displayMessage("\n!!! Nomor Polis tidak valid !!!\n");
+        execute();
       }
-   }
-    
-   
+
+    } 
 }
+
